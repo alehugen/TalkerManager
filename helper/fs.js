b696ff22edbf7633cf931212a7d9aae49065783f
Alessandro Hugen <alessandrohugen@gmail.com>
2022-01-17 14:06:23 -0300

    segunda parte dos requisitos

---
+++ helper/fs.js
@@ -0,0 +1,10 @@
+const fs = require('fs').promises;
+
+const readFileContent = async () => {
+  const fileContent = await fs.readFile('./talker.json');
+  return JSON.parse(fileContent);
+};
+
+module.exports = {
+  readFileContent,
+};
